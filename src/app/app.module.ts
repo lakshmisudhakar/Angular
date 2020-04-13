524f00e2c34498c9a8ed8f27c42b209cb5127c3a
lakshmisudhakar <lakshmisudhakarkamana@gmail.com>
2020-04-13 05:34:36 +0530

    completed with template-driven forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FomsComponent } from './foms/foms.component';
+import { TemplateFormsComponent } from './template-forms/template-forms.component';
+import { ReactiveModelFormComponent } from './reactive-model-form/reactive-model-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FomsComponent
+    FomsComponent,
+    TemplateFormsComponent,
+    ReactiveModelFormComponent
   ],
   imports: [
     BrowserModule,
